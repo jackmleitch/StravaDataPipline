4cc24d121d0954fea839d9a74eb2834c48528de8
jackmleitch <jackmleitch@gmail.com>
2022-06-17 14:53:41 +0100

    build: Project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="StravaDataPipeline",
+    packages=find_packages(),
+    version="0.1.0",
+    description="",
+    author="Jack Leitch",
+    license="MIT",
+)
