3e3f822f1b857565acf8c78e712e03ff129b2423
jackmleitch <jackmleitch@gmail.com>
2022-06-20 17:38:21 +0100

    fix: added .sql twice

--- src/validate_load_data.sh
+++ src/validate_load_data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python src/validator.py sql/validation/activity_dup.sql sql/validation/activity_dup_zero.sql equals warn
-python src/validator.py sql/validation/weekly_activity_count_zscore.sql sql/validation/zscore_90_twosided.sql.sql greater_equals warn
-python src/validator.py sql/validation/weekly_kudos_avg_zscore.sql sql/validation/zscore_90_twosided.sql.sql greater_equals warn
+python src/validator.py sql/validation/weekly_activity_count_zscore.sql sql/validation/zscore_90_twosided.sql greater_equals warn
+python src/validator.py sql/validation/weekly_kudos_avg_zscore.sql sql/validation/zscore_90_twosided.sql greater_equals warn
